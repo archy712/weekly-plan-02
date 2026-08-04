cb634ac1561a0365796e0b32098e9944e2a6f600
archy712 <archy712@gmail.com>
2026-08-04 11:18:51 +0900

    💄 style: 상단 헤더-본문 간격 축소

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -8,7 +8,7 @@ export default function ProtectedLayout({
 }) {
   return (
     <main className="min-h-screen flex flex-col items-center">
-      <div className="flex-1 w-full flex flex-col gap-20 items-center">
+      <div className="flex-1 w-full flex flex-col gap-8 items-center">
         <SiteHeader />
         <div className="flex-1 w-full flex flex-col gap-20 max-w-5xl p-5">
           {children}
