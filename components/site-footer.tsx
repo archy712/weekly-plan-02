feb1a002d41b5658c9033e040a8d8a6093cf8f69
archy712 <archy712@gmail.com>
2026-08-04 11:40:16 +0900

    💄 style: 풋터 문구를 Developed by archy712@gmail.com으로 변경

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -2,7 +2,7 @@ export function SiteFooter() {
   return (
     <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs py-16">
       <p>
-        Powered by <span className="font-bold">K2 Korea</span>
+        Developed by <span className="font-bold">archy712@gmail.com</span>
       </p>
     </footer>
   );
